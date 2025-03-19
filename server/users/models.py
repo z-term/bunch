d39c8dfecdad62b5642d73f415eb1b317a35a741
Harsh Narayan Jha <harshnj@proton.me>
2025-03-19 10:30:06 +0530

    server: Add email validation to User model

--- server/users/models.py
+++ server/users/models.py
@@ -2,6 +2,7 @@ import uuid
 from typing import override
 
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -10,6 +11,7 @@ class User(AbstractUser):
     Custom User Model for bunch.
     Adds additional properties like avatar, status, bio etc..
     """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to="avatars/", blank=True, null=False, help_text="User's profile picture")
@@ -19,6 +21,18 @@ class User(AbstractUser):
     @override
     def __str__(self) -> str:
         return self.username
+
+    @override
+    def clean(self):
+        if not self.email:
+            raise ValidationError("Email is required.")
+
+        super().clean()
+
+    @override
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
